4f0c62944b39020f3cfb2a5c0a8b6a536b133cc4
Monica Saeed <monicasaeed12@gmail.com>
2022-12-11 22:37:53 +0200

    update main

--- Schedulers/src/App.java
+++ Schedulers/src/App.java
@@ -1,9 +1,64 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);  
+
         ArrayList<SchedulerProcess> processes = new ArrayList<SchedulerProcess>();
+        System.out.print("Enter number of processes: ");
+        int num = sc.nextInt();
+        System.out.print("Enter round robin Time Quantum: ");
+        int RRQuantum = sc.nextInt();
+        System.out.print("Enter context switching: ");
+        int contextSwitching = sc.nextInt();
+        
+        for(int i=0;i<num;i++){
+            System.out.print("processes name: ");
+            String PName = sc.next();
+            System.out.print("arrival time: ");
+            int arrivalTime = sc.nextInt();
+            System.out.print("burst time: ");
+            int burstTime = sc.nextInt();
+            System.out.print("priority: ");
+            int priority = sc.nextInt();
+            processes.add( new SchedulerProcess(PName, arrivalTime, burstTime, priority) );
+        }
+
+
+        System.out.println("Enter 1 for SRTFScheduler, 2 for RRScheduler, 3 for PriorityScheduler");
+        int test = sc.nextInt();
+
+        do{
+
+            Collections.sort(processes, new ProcessComparator());
+            Scheduler s;
+
+            if(test == 1){
+                s = new SRTFScheduler(processes, contextSwitching);
+                s.schedule();
+                s.displayAllData();
+            }else if(test == 2){
+                s = new RRScheduler(processes, contextSwitching, RRQuantum);
+                s.schedule();
+                s.displayAllData();
+            }else{
+                s = new PriorityScheduler(processes);
+                s.schedule();
+                s.displayAllData();
+            }
+
+            System.out.println("Enter 1 for SRTFScheduler, 2 for RRScheduler, 3 for PriorityScheduler");
+            test = sc.nextInt();
+
+        }while(test != -1);
+
+
+
+
+
+        /* 
         processes.add(new SchedulerProcess("P1", 0, 1, 1));
         processes.add(new SchedulerProcess("P2", 1, 7, 2));
         processes.add(new SchedulerProcess("P3", 2, 3, 3));
@@ -32,6 +87,6 @@ public class App {
         s = new PriorityScheduler(processes);
         System.out.println("\nPreemptive Priority Scheduling (with the solving of starvation problem)");
         s.schedule();
-        s.displayAllData();
+        s.displayAllData();*/
     }
 }
